5a03f0dfb390800f51d775adc134bf903c0fb5fc
Alex DeGrave <ajd98@pitt.edu>
2022-01-20 17:42:03 -0800

    initial commit

---
+++ interact.js
@@ -0,0 +1,10 @@
+document.getElementById("emailLogo").onclick = function () {
+  p1 = "degr"
+  p2 = "ave"
+  p3 = "@"
+  p4 = "uw.e"
+  p5 = "du"
+  window.location = "mailto:"+p1+p2+p3+p4+p5;
+}
+
+document.getElementById("mail").innerHTML = "degrave" + "@" + "uw.edu";
