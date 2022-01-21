7b4849b21f71f93415c8f2371480b526f2086ba8
Alex DeGrave <ajd98@pitt.edu>
2022-01-21 10:29:32 -0800

    fix null errors

--- interact.js
+++ interact.js
@@ -1,10 +1,13 @@
 document.getElementById("emailLogo").onclick = function () {
-  p1 = "degr"
-  p2 = "ave"
-  p3 = "@"
-  p4 = "uw.e"
-  p5 = "du"
+  p1 = "degr";
+  p2 = "ave";
+  p3 = "@";
+  p4 = "uw.e";
+  p5 = "du";
   window.location = "mailto:"+p1+p2+p3+p4+p5;
 }
 
-document.getElementById("mail").innerHTML = "degrave" + "@" + "uw.edu";
+mail = document.getElementById("mail");
+if (mail) {
+  mail.innerHTML = "degrave" + "@" + "uw.edu";
+}
